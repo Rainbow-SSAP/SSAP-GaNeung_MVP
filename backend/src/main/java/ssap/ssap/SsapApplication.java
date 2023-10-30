e75a7457be17d602168f0de4b68d0efd67781a6e
ssap-rainbow <ssap.rainbow@gmail.com>
2023-10-30 16:57:12 +0900

    build: 프로젝트 초기 개발 환경 셋팅
    프로젝트 초기 개발 환경 셋팅

---
+++ backend/src/main/java/ssap/ssap/SsapApplication.java
@@ -0,0 +1,13 @@
+package ssap.ssap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsapApplication.class, args);
+	}
+
+}

fac50b168e46a3c108ae8a927bc4dd52a15bb13d
Seongsuk Hong <ssukh77@naver.com>
2023-11-03 11:44:35 +0900

    design(초기 CSS 세팅하기): #12
    GlobalStyles, styled-normalize, ThemeProvider 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- frontend/src/styles/globalStyle.ts
+++ frontend/src/styles/globalStyle.ts
@@ -1,1 +1,64 @@
-export {};
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+const styled = { createGlobalStyle };
+
+const GlobalStyle = styled.createGlobalStyle`
+  ${normalize}
+
+  @font-face {
+    font-family: "Noto Sans KR", sans-serif;
+    src: url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap");
+  }
+
+  * {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  html,
+  body,
+  div,
+  span,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  form,
+  label,
+  table {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+
+  body {
+    font-family: "Noto Sans KR", sans-serif;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;

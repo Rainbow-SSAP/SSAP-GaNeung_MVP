908020f0f00ee81e16a8dfb727ba5918c8960265
Junsu Park <qkrwnstn95@naver.com>
2023-11-07 11:41:44 +0900

    docs(메인페이지 카테고리 목록 조회 기능 #4):
    Category, CategoryController, CategoryRepository emend
    
    Closes #4

--- backend/src/main/java/ssap/ssap/repository/CategoryRepository.java
+++ backend/src/main/java/ssap/ssap/repository/CategoryRepository.java
@@ -3,7 +3,5 @@ package ssap.ssap.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ssap.ssap.dto.Category;
 
-import java.util.List;
-
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }

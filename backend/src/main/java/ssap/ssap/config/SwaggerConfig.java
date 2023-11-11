7c100d5133904b45412630f849b7057d85bc939c
cipp00h <12seokeun@gmail.com>
2023-11-11 21:58:14 +0900

    feature: [BE] Swagger에서 API 호출 시 CORS 에러 발생 #37
    OPenAPI에 기본 서버 주소를 설정하여 해결
    
    Closes #37

--- backend/src/main/java/ssap/ssap/config/SwaggerConfig.java
+++ backend/src/main/java/ssap/ssap/config/SwaggerConfig.java
@@ -2,12 +2,15 @@ package ssap.ssap.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(title = "SSAP 심부름 플랫폼 API",
                 description = "SSAP 심부름 플랫폼 API입니다.",
-                version = "v1"))
+                version = "v1"),
+        servers = @Server(url = "/", description = "Default Server URL")
+)
 
 @Configuration
 public class SwaggerConfig {

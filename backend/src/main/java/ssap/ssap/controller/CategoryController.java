5945db8d81241cd33d23610bbc4d83b394b54bea
Junsu Park <qkrwnstn95@naver.com>
2023-11-14 18:49:27 +0900

    docs: token검증 추가

--- backend/src/main/java/ssap/ssap/controller/CategoryController.java
+++ backend/src/main/java/ssap/ssap/controller/CategoryController.java
@@ -2,11 +2,15 @@ package ssap.ssap.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ssap.ssap.dto.Category;
 import ssap.ssap.service.CategoryService;
+import ssap.ssap.service.OAuthService;
 
 import java.util.List;
 
@@ -16,14 +20,27 @@ import java.util.List;
 public class CategoryController {
 
     private final CategoryService categoryService;
+    private final OAuthService oAuthService;
 
     @Autowired
-    public CategoryController(CategoryService categoryService){
+    public CategoryController(CategoryService categoryService, OAuthService oAuthService){
         this.categoryService = categoryService;
+        this.oAuthService = oAuthService;
     }
 
     @GetMapping("/categories")
-    public List<Category> getCategories(){
-        return categoryService.getCategories();
+    public ResponseEntity<?> getCategories(@RequestHeader("Authorization")String authorizationHeader){
+        try {
+            String accessToken = authorizationHeader.substring("Bearer ".length());
+            boolean isValid = oAuthService.isAccessTokenValid(accessToken);
+            if (isValid) {
+                List<Category> categories = categoryService.getCategories();
+                return ResponseEntity.ok(categories);
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("토큰 검증 중 오류가 발생했습니다.");
+            }
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("토큰 검증 중 오류가 발생했습니다.");
+        }
     }
 }

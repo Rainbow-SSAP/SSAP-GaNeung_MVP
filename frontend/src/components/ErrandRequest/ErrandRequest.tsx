9a5254f057161111476ffb9dcc6d2a712b174697
Seongsuk Hong <ssukh77@naver.com>
2023-11-14 21:12:28 +0900

    feat(Title(제목) 작성 로직 추가): #49
    제목 필드 추가 및 유효성 검사와 에러 처리 구현하였습니다.

--- frontend/src/components/ErrandRequest/ErrandRequest.tsx
+++ frontend/src/components/ErrandRequest/ErrandRequest.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import Category from "./Category/Category";
+import Title from "./Title/Title";
 
 export const ErrandRequest = () => {
   return (
     <ErrandRequestWarpper>
       <Category />
+      <Title />
     </ErrandRequestWarpper>
   );
 };

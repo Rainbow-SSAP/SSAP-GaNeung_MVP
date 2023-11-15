0bb34b57cc69fad08bb8fdb747fcb436de5a6fc0
Seongsuk Hong <ssukh77@naver.com>
2023-11-15 17:30:36 +0900

    feat(Description(요청 내용) 작성 로직 추가): #57
    상세 요청 내용 필드 추가 및 유효성 검사와 에러 처리 구현하였습니다.

--- frontend/src/components/ErrandRequest/ErrandRequest.tsx
+++ frontend/src/components/ErrandRequest/ErrandRequest.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Category from "./Category/Category";
 import Title from "./Title/Title";
+import Description from "./Description/Description";
 
 export const ErrandRequest = () => {
   return (
     <ErrandRequestWarpper>
       <Category />
       <Title />
+      <Description />
     </ErrandRequestWarpper>
   );
 };

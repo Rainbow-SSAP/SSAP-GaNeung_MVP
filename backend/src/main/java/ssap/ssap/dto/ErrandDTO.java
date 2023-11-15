d785afadb9b5ede770965c8274e591439dd58c43
Junsu Park <qkrwnstn95@naver.com>
2023-11-15 17:48:58 +0900

    docs: merge 충돌나서 다시 만들었습니다..

---
+++ backend/src/main/java/ssap/ssap/dto/ErrandDTO.java
@@ -0,0 +1,25 @@
+package ssap.ssap.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+public class ErrandDTO {
+    private Long taskId;
+    private String title;
+    private String description;
+    private String creationTime;
+    private String auctionEndTime;
+    private String thumbnailUrl;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    // LocalDateTime을 받아 String으로 변환하여 저장
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime != null ? creationTime.format(formatter) : null;
+    }
+    public void setAuctionEndTime(LocalDateTime auctionEndTime) {
+        this.auctionEndTime = auctionEndTime != null ? auctionEndTime.format(formatter) : null;
+    }
+}
---
+++ backend/src/main/java/ssap/ssap/service/ErrandService.java
@@ -0,0 +1,45 @@
+package ssap.ssap.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ssap.ssap.domain.ErrandEntity;
+import ssap.ssap.dto.ErrandDTO;
+import ssap.ssap.repository.ErrandRepository;
+import ssap.ssap.repository.ThumbNailRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ErrandService {
+    private final ErrandRepository errandRepository;
+    private final ThumbNailRepository thumbNailRepository;
+    public ErrandService(ErrandRepository errandRepository, ThumbNailRepository thumbNailRepository) {
+        this.errandRepository = errandRepository;
+        this.thumbNailRepository = thumbNailRepository;
+    }
+    public List<ErrandDTO> findAllErrands() {
+        List<ErrandEntity> errands = errandRepository.findAll();
+        return errands.stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
+    private ErrandDTO convertToDTO(ErrandEntity errand) {
+        ErrandDTO dto = new ErrandDTO();
+        dto.setTaskId(errand.getId());
+        dto.setTitle(errand.getTitle());
+        dto.setDescription(errand.getDescription());
+        dto.setCreationTime(errand.getCreationTime());
+        dto.setAuctionEndTime(errand.getAuctionEndTime());
+        if (errand.getAttachment() != null) {
+            dto.setThumbnailUrl(errand.getAttachment().getFileData());
+        }
+        return dto;
+    }
+    @Transactional(readOnly = true)
+    public Page<ErrandDTO> findAllErrands(Pageable pageable) {
+        return errandRepository.findAll(pageable)
+                .map(this::convertToDTO); // 페이지를 DTO로 변환합니다.
+    }
+    // 기타 CRUD 메소드 작성해야함. 현재는 조회만 가능.
+}

476a062d1c5769e2c29dcd3acba2825d06ba3d28
Junsu Park <qkrwnstn95@naver.com>
2023-11-20 16:19:47 +0900

    docs : 테이블 명 _ 추가

--- backend/src/main/java/ssap/ssap/domain/DetailItem.java
+++ backend/src/main/java/ssap/ssap/domain/DetailItem.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "DetailedItems")
+@Table(name = "detailed_items")
 public class DetailItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

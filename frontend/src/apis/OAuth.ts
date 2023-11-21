6734600c08661a5acdb1b2cc1f5404c8a82a2147
장정안 <wjddks96@naver.com>
2023-11-21 18:58:11 +0900

    feat: redirect URI 인코딩으로 변경

--- frontend/src/apis/OAuth.ts
+++ frontend/src/apis/OAuth.ts
@@ -1,5 +1,7 @@
-const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
+const CLIENT_ID = process.env.REACT_APP_JAVASCRIPT_API_KEY;
+const REDIRECT_URI: string = process.env.REACT_APP_REDIRECT_URI || "";
 
-const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+const encodedRedirectUri = encodeURIComponent(REDIRECT_URI);
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodedRedirectUri}&response_type=code`;
+// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=8fff620ae3c08a65ad853f0562846d38&redirect_uri=http://localhost:3000/login/oauth2/code/kakao&response_type=code`;

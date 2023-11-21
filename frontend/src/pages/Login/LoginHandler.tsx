5b8a8e14c2c4d472c80e7767d5491c17dd562e50
장정안 <wjddks96@naver.com>
2023-11-21 18:52:13 +0900

    feat: 로그인 핸들러 구현

--- frontend/src/pages/Login/LoginHandler.tsx
+++ frontend/src/pages/Login/LoginHandler.tsx
@@ -1,37 +1,48 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export const LoginHandeler = () => {
-  //인가코드 백으로 전달하는 곳
+export const LoginHandler = () => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
-  console.log("code", code);
+  console.log("code 받음", code);
+  // 사용자 이름과 이메일을 상태로 관리
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
   useEffect(() => {
-    const kakaoLogin = async () => {
-      await axios({
-        method: "GET",
-        url: `${process.env.REACT_APP_REDIRECT_URL}/?code=${code}`,
-        headers: {
-          "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
-          "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
-        },
-      }).then((res) => {
-        //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-        console.log(res);
-        //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
-        localStorage.setItem("name", res.data.account.kakaoName);
-        //로그인이 성공하면 이동할 페이지
-        navigate("/");
-      });
-    };
     kakaoLogin();
-  }, []);
-  return (
-    <div>
-      <p>로그인 중입니다.</p>
-      <p>잠시만 기다려주세요.</p>
-    </div>
-  );
+  }, [code]);
+
+  const kakaoLogin = async () => {
+    try {
+      console.log("서버에 코드 요청 보냄", code);
+      const res = await axios({
+        method: "GET",
+        url: `/api/oauth/kakao/callback?code=${code}`,
+        // url: `https://k1e5f1ddb1135a.user-app.krampoline.com/api/oauth/kakao/callback?code=${code}`,
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+      });
+      console.log("서버로부터 응답", res);
+
+      // 백엔드 응답 처리
+      if (res.data.loginSuccess) {
+        console.log("로그인 성공", res.data);
+        const { userName, userEmail } = res.data.account;
+        setUserName(userName); // 상태 업데이트
+        setUserEmail(userEmail); // 상태 업데이트
+        sessionStorage.setItem("accessToken", res.data.accessToken); // 액세스 토큰 저장
+        console.log("네비게이팅");
+        navigate("/home"); // HomePage로 이동
+      } else {
+        console.error("로그인 실패", res.data);
+      }
+    } catch (error) {
+      console.error("로그인 요청 실패:", error);
+    }
+  };
+
+  return <div>로그인 중입니다...</div>;
 };

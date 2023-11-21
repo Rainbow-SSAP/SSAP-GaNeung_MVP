9609e9afbc8ed921628e5fa2a2132d945b4a5811
장정안 <wjddks96@naver.com>
2023-11-21 18:52:21 +0900

    feat: 로그인 페이지 구현

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { KAKAO_AUTH_URL } from "../../apis/OAuth";
 import kakaoImage from "../../assets/Kakao.png";
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
   return (
+    // <Link to={KAKAO_AUTH_URL} className="kakaobtn">
+    //   <img src={kakaoImage} alt="kakao login btn" />
+    // </Link>
     <a href={KAKAO_AUTH_URL} className="kakaobtn">
       <img src={kakaoImage} alt="kakao login btn" />
     </a>

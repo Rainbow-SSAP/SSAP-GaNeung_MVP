c797fda6dbcd66ff12a771375a9db1b11c4d989d
장정안 <wjddks96@naver.com>
2023-11-21 20:18:50 +0900

    feat: accessToken 인터셉트 여부 확인 콘솔

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -10,10 +10,14 @@ api.interceptors.request.use(
   async (config) => {
     // 엑세스 토큰을 세션 스토리지에서 가져오기
     const accessToken = sessionStorage.getItem("accessToken");
-
+    console.log("accessToken 인터셉트", accessToken);
     // 토큰이 있는 경우 헤더에 추가
     if (accessToken) {
       config.headers["Authorization"] = `Bearer ${accessToken}`;
+      console.log(
+        "Authorization 헤더에 토큰이 제대로 설정됨",
+        config.headers["Authorization"],
+      );
     }
 
     return config;

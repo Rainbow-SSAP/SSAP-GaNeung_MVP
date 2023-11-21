649b7c70f46bc509411b3ecb8b4ae17ea1305c9b
Seongsuk Hong <ssukh77@naver.com>
2023-11-21 17:51:29 +0900

    feat(요청서 작성 API 호출 추가): #92

---
+++ frontend/src/apis/errand.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { ErrandFormData } from "../types/errand";
+
+const APP_URL = process.env.REACT_APP_URL;
+const accessToken = process.env.REACT_APP_ACCESSTOKEN;
+const userEmail = "ssap.rainbow@gmail.com";
+
+// 심부름 요청 데이터 보내기
+export const ErrandRequestPost = async (errandFormData: ErrandFormData) => {
+  const formData = {
+    ...errandFormData,
+    email: userEmail, // 이메일 추가
+  };
+  console.log("ErrandRequestPost", formData);
+  try {
+    const response = await axios.post(`${APP_URL}/api/request`, formData, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+        // TODO 이미지 파일 타입 보낼 때 수정 필요
+        // 현재 api 호출시 multipart 타입을 지원하지 않음..
+        // "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    // 오류 처리
+    console.error(error);
+    throw error;
+  }
+};

d195a182b328e12c718b24724031c677d23c4311
장정안 <wjddks96@naver.com>
2023-11-21 18:16:35 +0900

    fix: 심부름 요청페이지 내용 삭제 (파일 헷갈림 이슈)
    심부름 상세 페이지에서 작업해야 하는 것을, 심부름 요청페이지에서 작업했던 관계로,
    merge할 때 충돌날 경우를 대비하여 내용을 삭제하고 기본 틀만 남겨놨습니다

--- frontend/src/pages/ErrandRequest/ErrandRequestPage.tsx
+++ frontend/src/pages/ErrandRequest/ErrandRequestPage.tsx
@@ -1,51 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-import { FormProvider, useForm } from "react-hook-form";
-import { Button } from "../../components/@common/Button/Button";
-import { ErrandRequest } from "../../components/ErrandRequest/ErrandRequest";
-import { ErrandFormData } from "../../types/errand";
-import { categories } from "../../constants/errand";
 
 const ErrandRequestPage = () => {
-  const methods = useForm<ErrandFormData>({
-    defaultValues: {
-      // 기본값
-      category: categories[0].value,
-      detailedItem: categories[0].detailedItems
-        ? categories[0].detailedItems[0].value
-        : "",
-    },
-  });
-
-  const {
-    register,
-    control,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = methods;
-
-  const onSubmit = (data: ErrandFormData) => {
-    // 폼 제출 시 실행될 로직
-    console.log(data);
-  };
-
-  return (
-    <div>
-      <FormWapper onSubmit={methods.handleSubmit(onSubmit)}>
-        {/* useForm 훅에서 반환된 메서드를 자식 컴포넌트로 전달합니다 */}
-        <FormProvider {...methods}>
-          <ErrandRequest />
-          <Button text="🚨 심부름 요청하기" type="submit" />
-        </FormProvider>
-      </FormWapper>
-    </div>
-  );
+  return <div></div>;
 };
-
-const FormWapper = styled.form`
-  display: grid;
-  grid-gap: 20px;
-`;
 
 export default ErrandRequestPage;

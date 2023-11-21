351335c53df3e9fdbc63264f00fb28b0fff8c749
장정안 <wjddks96@naver.com>
2023-11-21 14:33:05 +0900

    add: close 버튼 추가

--- frontend/src/assets/headerImages.ts
+++ frontend/src/assets/headerImages.ts
@@ -1,9 +1,11 @@
 import goback from "./goback.png";
 import menu from "./menu.png";
 import menu2 from "./menu2.png";
+import close from "./close.png";
 
 export const headerImage = {
   goback,
   menu,
   menu2,
+  close,
 };

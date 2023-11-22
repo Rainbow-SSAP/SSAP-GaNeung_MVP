35f52427b2fed083cf96071edf1d2ddb2b992c6e
Seongsuk Hong <ssukh77@naver.com>
2023-11-23 00:49:01 +0900

    feat(메인페이지 헤더 추가 및 메뉴 버튼 선택 시 toggle 구현): #109
    메인페이지 헤더 추가 및 메뉴 버튼 선택 시 toggle 구현하였습니다.
    - MainHeader 컴포넌트 추가:
       - 로고 클릭 시 home으로 라우터 처리
       - 메뉴 열림/닫힘 리코일로 상태관리해서 props로 Menu 컴포넌트로 보냄
       - css 스타일링
    - isOpen 상태에 따라 position 값 적용
    - 메뉴 열림 시 body 스크롤 방지

--- frontend/src/components/@common/Header/Menu/Menu.tsx
+++ frontend/src/components/@common/Header/Menu/Menu.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserProfile from "./UserProfile";
 import styled from "styled-components";
 import Nav from "./Nav";
 
-function Menu() {
+interface MenuProps {
+  isOpen: boolean;
+}
+
+function Menu({ isOpen }: MenuProps) {
+  useEffect(() => {
+    if (isOpen) {
+      // 메뉴가 열려있으면 body 스크롤을 비활성화
+      document.body.style.overflow = "hidden";
+    } else {
+      // 메뉴가 닫혀있으면 스크롤을 다시 활성화
+      document.body.style.overflow = "";
+    }
+  }, [isOpen]);
 
   return (
-    <MenuContainer>
+    <MenuContainer isOpen={isOpen}>
       <MenuContents>
         <UserProfile />
         <Nav />
@@ -15,7 +28,16 @@ function Menu() {
   );
 }
 
-const MenuContainer = styled.aside`
+const MenuContainer = styled.aside<MenuProps>`
+  position: fixed;
+  top: 0;
+  right: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+  width: 100%;
+  height: 100vh;
+  z-index: 10;
+  background-color: white;
+  transition: right 0.3s;
+  overflow-y: auto;
 `;
 const MenuContents = styled.div`
   padding: 0 5vw;

551020392cd6b8dd8669a4cc53e38f7f96f5b4e5
Seongsuk Hong <ssukh77@naver.com>
2023-11-23 01:26:12 +0900

    feat(메인 카테고리 선택하기 추가): #112
    메인에서 카테고리 선택하면 라우터 처리되도록 구현하였습니다.
    - Home 컴포넌트에서 Main 컴포넌트로 변경 (main으로 계속 작업하다보니 헷갈려서..ㅎㅎ)
    - Category파일 Main 파일 안으로 위치 변경
    - 카테고리 api 데이터 받아와서 아이콘이랑 매칭해서 CategoryItem에 props로 보냄
    - 카테고리 클릭 시 라우터 처리

---
+++ frontend/src/components/Main/Main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CategoryGrid from "./Category/CategoryGrid";
+import Errands from "../ErrandList/Errands";
+import styled from "styled-components";
+
+function Main() {
+  return (
+    <MainWrapper>
+      <CategoryWrapper>
+        <CategoryGrid />
+      </CategoryWrapper>
+    </MainWrapper>
+  );
+}
+
+const MainWrapper = styled.section`
+  width: 100%;
+`;
+
+const CategoryWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.color.lightPurple};
+`;
+
+export default Main;

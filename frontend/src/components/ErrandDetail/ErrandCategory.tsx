9a5dba31e23695db115ae4e6a806d8a28c5deff0
장정안 <wjddks96@naver.com>
2023-11-23 23:21:17 +0900

    design: 상세 페이지 내 카테고리 ui 부분 구현(곧 작업 예정)

---
+++ frontend/src/components/ErrandDetail/ErrandCategory.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const ErrandCategory = ({ data }) => {
+  return <Layout></Layout>;
+};
+
+export default ErrandCategory;
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;

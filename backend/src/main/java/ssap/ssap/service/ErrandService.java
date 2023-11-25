18e394b474040d715772c509e43e2b037af4623e
Junsu Park <qkrwnstn95@naver.com>
2023-11-25 17:39:02 +0900

    feature : 메인페이지 🌈 우리 동네 심부름 리스트 동(지번) 별 필터링 구현 #171
    메인페이지 동(지번) 별 필터링 구현

--- backend/src/main/java/ssap/ssap/service/ErrandService.java
+++ backend/src/main/java/ssap/ssap/service/ErrandService.java
@@ -20,6 +20,18 @@ public class ErrandService {
         this.taskRepository = taskRepository;
         this.taskAttachmentRepository = taskAttachmentRepository;
     }
+
+    @Transactional(readOnly = true)
+    public Page<ErrandResponseDto> getErrands(String address, Pageable pageable) {
+        if (address == null || address.isEmpty()) {
+            // 주소 정보가 없는 경우, 전체 심부름 목록을 반환
+            return findAllErrands(pageable);
+        } else {
+            // 주소 정보가 있는 경우, 위치 기반으로 필터링된 심부름 목록을 반환
+            return getFilteredErrands(address, pageable);
+        }
+    }
+
     @Transactional(readOnly = true)
     public Page<ErrandResponseDto> findAllErrands(Pageable pageable) {
         return taskRepository.findAll(pageable)
@@ -47,7 +59,16 @@ public class ErrandService {
             dto.setFileData(firstAttachment.getFileData());
         }
     }
+    @Transactional(readOnly = true)
+    public Page<ErrandResponseDto> getFilteredErrands(String address, Pageable pageable) {
+        String district = extractDistrictFromAddress(address);
+        if (district == null) {
+            return Page.empty(); // '동'을 찾지 못한 경우 빈 페이지 반환
+        }
 
+        Page<Task> taskPage = taskRepository.findByJibunAddressContaining(district, pageable);
+        return taskPage.map(this::convertToDto);
+    }
     // 주소에서 '동'을 추출하는 메소드
     public String extractDistrictFromAddress(String fullAddress) {
         if(fullAddress == null || fullAddress.isEmpty()) {

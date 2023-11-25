25aca894e1e4f56d180fc1f00a046c6b07afabb9
장정안 <wjddks96@naver.com>
2023-11-26 00:39:52 +0900

    feat: 카테고리 리스트 가져오는 api 호출 함수를 axios로 작성

--- frontend/src/apis/category.ts
+++ frontend/src/apis/category.ts
@@ -1,15 +1,17 @@
+import axios from "axios";
 import { APP_URL, accessToken } from "./OAuth";
 import api from "./api";
 
 // 카테고리 리스트 가져오기
 export const getCategories = async () => {
   try {
-    const response = await api.get(`${APP_URL}/api/categories`, {
+    const response = await axios.get(`/api/categories`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         // "Content-Type": "application/json",
       },
     });
+    console.log("category.ts 안 response.data:", response.data);
     return response.data;
   } catch (error) {
     // 오류 처리
@@ -21,7 +23,7 @@ export const getCategories = async () => {
 // 하위 카테고리 리스트 가져오기
 export const getSubCategories = async (categoryId) => {
   try {
-    const response = await api.get(`${APP_URL}/api/categories/${categoryId}/items`, {
+    const response = await axios.get(`/api/categories/${categoryId}/items`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         // "Content-Type": "application/json",

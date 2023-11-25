97725757ff9287e2d34b2e62eb5e605f7d2c95dd
Seongsuk Hong <ssukh77@naver.com>
2023-11-26 02:23:29 +0900

    design(Header, GnbHeader, DateInput 컴포넌트 css 수정)

--- frontend/src/components/ErrandRequest/DateInput/DateInput.tsx
+++ frontend/src/components/ErrandRequest/DateInput/DateInput.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 import { FaCalendar } from "react-icons/fa";
 import { GoDash } from "react-icons/go";
 import { ko } from "date-fns/esm/locale";
@@ -57,10 +57,11 @@ function DateInput({
           selected={startDate} // 시작 날짜
           dateFormat="yyyy-MM-dd HH:mm" // 2023-11-20 14:00 형식
           showTimeSelect // 시간 설정
+          minDate={new Date()}
           filterTime={filterPassedTime} // 이전 시간 필터링
-          closeOnScroll={true} // 스크롤을 움직였을 때 자동으로 닫히도록 설정 기본값 false
           onChange={handleStartDateChange}
           customInput={<CustomInput />}
+          withPortal
         />
         <StyledIcon />
       </DateInputBox>
@@ -75,9 +76,9 @@ function DateInput({
           minDate={startDate} // 시작 날짜 이후의 날짜만 선택 가능하도록 설정
           minTime={startDate}
           maxTime={setHours(setMinutes(new Date(), 59), 23)}
-          closeOnScroll={true} // 스크롤을 움직였을 때 자동으로 닫히도록 설정 기본값 false
           onChange={(date: Date) => setEndDate(date)}
           customInput={<CustomInput />}
+          withPortal
         />
         <StyledIcon />
       </DateInputBox>
@@ -95,15 +96,85 @@ const DateInputBox = styled.div`
   align-items: center;
   flex: 1;
 
-  > .react-datepicker-wrapper {
+  > div {
     width: 100%;
   }
-  .react-datepicker-popper {
-    display: flex;
+
+  .react-datepicker-wrapper {
+    width: 100%;
+    div {
+      width: 100%;
+    }
+  }
+  .react-datepicker__portal {
+    // 모달 Bg
+    background-color: transparent;
   }
   .react-datepicker-popper[data-placement^="bottom"]
     .react-datepicker__triangle {
     display: none;
+  }
+  .react-datepicker {
+    // 날짜 시간 컨테이너
+    // 날짜
+    display: flex;
+    /* border: none; */
+    border-radius: 1rem;
+    background-color: white;
+    overflow: hidden;
+  }
+  .react-datepicker__month-container {
+    // 날짜 컨테이너
+    float: none;
+    padding: 0 8px;
+    padding-bottom: 10px;
+  }
+  .react-datepicker__header {
+    // 날짜 헤더
+    background-color: white;
+    border-bottom: none;
+  }
+  .react-datepicker__current-month {
+    padding: 1rem;
+  }
+
+  // 선택된 날짜
+  .react-datepicker__day--selected,
+  .react-datepicker__day--in-selecting-range,
+  .react-datepicker__day--in-range,
+  .react-datepicker__month-text--selected,
+  .react-datepicker__month-text--in-selecting-range,
+  .react-datepicker__month-text--in-range,
+  .react-datepicker__quarter-text--selected,
+  .react-datepicker__quarter-text--in-selecting-range,
+  .react-datepicker__quarter-text--in-range,
+  .react-datepicker__year-text--selected,
+  .react-datepicker__year-text--in-selecting-range,
+  .react-datepicker__year-text--in-range {
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.color.primary};
+  }
+
+  // nav arrow
+  .react-datepicker__navigation {
+    top: 12px;
+  }
+  .react-datepicker__navigation--previous {
+    margin-left: 10px;
+  }
+  .react-datepicker__navigation--next {
+    margin-right: 10px;
+  }
+
+  // 시간 컨테이너
+  .react-datepicker__time-container {
+    /* height: 280px; */
+  }
+  .react-datepicker__header--time {
+    padding: 1.6rem;
+  }
+  .react-datepicker__time-list {
+    /* height: auto !important; */
   }
 `;
 
@@ -113,13 +184,14 @@ const StyledDatePicker = styled(DatePicker)`
   padding: 10px;
   border-radius: 4px;
   font-size: 12px;
-  border: 1px solid #f4f4f4;
+  border: none;
   background-color: white;
 
   > .custom_input {
     width: 100%;
   }
 `;
+
 const StyledCustomInput = styled.div`
   cursor: pointer;
   width: 100%;

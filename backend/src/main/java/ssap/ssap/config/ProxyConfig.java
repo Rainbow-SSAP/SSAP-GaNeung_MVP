e746eff166adfb9207a26693ecaae09c81682b8e
cipp00h <12seokeun@gmail.com>
2024-01-31 22:01:15 +0900

    chore: 프록시 설정 관련 코드 주석 처리 #230
    - proxy 설정 코드 주석 처리
    
    Closes #230

--- backend/src/main/java/ssap/ssap/config/ProxyConfig.java
+++ backend/src/main/java/ssap/ssap/config/ProxyConfig.java
@@ -10,11 +10,11 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ProxyConfig {
-    @Value("${PROXY_HOST:defaultProxyHost}")
-    private String proxyHost;
+//    @Value("${PROXY_HOST:defaultProxyHost}")
+//    private String proxyHost;
 
-    @Value("${PROXY_PORT:8080}")
-    private int proxyPort;
+//    @Value("${PROXY_PORT:8080}")
+//    private int proxyPort;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -22,10 +22,10 @@ public class ProxyConfig {
         var clientBuilder = HttpClients.custom();
 
         // 프록시 호스트가 설정되어 있다면 프록시를 적용합니다.
-        if (!proxyHost.isEmpty()) {
-            HttpHost proxy = new HttpHost(proxyHost, proxyPort);
-            clientBuilder.setProxy(proxy);
-        }
+//        if (!proxyHost.isEmpty()) {
+//            HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+//            clientBuilder.setProxy(proxy);
+//        }
 
         // HttpClient 인스턴스 생성
         var httpClient = clientBuilder.build();
